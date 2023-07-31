cad9ff92adcb5a9b9b7df0c00b18e33034ff7518
Panda77 <PiperZzz@users.noreply.github.com>
2023-07-31 16:35:13 -0400

    logger

--- src/main/java/com/example/eventstore/EventstoreApplication.java
+++ src/main/java/com/example/eventstore/EventstoreApplication.java
@@ -3,11 +3,16 @@ package com.example.eventstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication
 public class EventstoreApplication {
+	static Logger logger = LoggerFactory.getLogger(EventstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventstoreApplication.class, args);
+		logger.info("Eventstore Ready!");
 	}
 
 }
