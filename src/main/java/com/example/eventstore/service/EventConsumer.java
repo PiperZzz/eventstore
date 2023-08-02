860408ee276129a5d507279404b446111bffebf6
Panda77 <PiperZzz@users.noreply.github.com>
2023-08-02 14:25:57 -0400

    kafka init

---
+++ src/main/java/com/example/eventstore/service/EventConsumer.java
@@ -0,0 +1,17 @@
+package com.example.eventstore.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Service
+public class EventConsumer {
+    static Logger logger = LoggerFactory.getLogger(EventConsumer.class);
+
+    @KafkaListener(topics = "trading-events", groupId = "eventstore-group")
+    public void consume(String message) {
+        logger.info("Received event: {}", message);
+    }
+}
